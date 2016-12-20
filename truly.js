0b2079d742edb1a1d67be098f412c39ef97f5c23
johnlenon <johnlenonmaghanoy@gmail.com>
2016-12-20 11:19:51 +0800

    Improved build procedure.

--- truly.js
+++ truly.js
@@ -34,6 +34,9 @@
 			"file": "truly.js",
 			"module": "truly",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/truly.git",
 			"test": "truly-test.js",
